5097defa6a20947164e374746177de217f890f59
Kirill <kirill@maincard.io>
2025-02-16 08:41:24 +0300

    lola

--- packages/components/Button/src/Button.tsx
+++ packages/components/Button/src/Button.tsx
@@ -1,9 +1,3 @@
-import styled from "styled-components";
-
-const Styled = styled("div")(({ theme }) => ({
-  background: theme.borderRadius,
-}));
-
 export const Button = () => {
   return <div>Button</div>;
 };
