e0271b8231836b747dbfe2f6a4ec382273a7e612
Kirill <kirill@maincard.io>
2025-02-16 06:01:32 +0300

    fix: arividerchi

--- packages/components/Button/src/Button.tsx
+++ packages/components/Button/src/Button.tsx
@@ -1,6 +1,8 @@
 import { theme } from "@supremes-ui/theme";
 import styled from "styled-components";
 
+console.log("ARIVIDERCHI");
+
 const Styled = styled("div")(({ theme }) => ({
   background: theme.borderRadius,
 }));
